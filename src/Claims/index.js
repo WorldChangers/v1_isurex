ec22d30d597729ef4cb6b48f1a4944dcc0f79cf5
Terrence <owusuterrence@gmail.com>
2018-03-02 02:52:41 +0000

    disabled the view to render the add client page

--- src/Claims/index.js
+++ src/Claims/index.js
@@ -5,6 +5,15 @@ import Loader from '../Loader'
 
 class Welcome extends PureComponent {
 
+    async componentDidMount(){
+        const res = await localStorage.getItem('user')
+        const user = JSON.parse(res)
+
+        if(user.clients.length === 0){
+            return this.props.history.push('/clients/new')
+        }
+    }
+
     render(){
         return (
             <Home location={this.props.location}>
